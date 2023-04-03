70ae5af281f61a8d03684755436b4c8eb742551b
Yousef mostafa <yousef.moustafa@bevatel.com>
2023-04-03 14:27:44 +0200

    handle login with token use jwt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
+    <div class="info">
+      <h2 class="mb-3">{{ welcomeText }}</h2>
+      <p class="error text-danger">{{ error }}</p>
+    </div>
     <div class="d-flex justify-content-center">
-      <form @submit.prevent="submitToken()">
-        <p>Enter room token</p>
+      <form @submit.prevent="submitToken()" v-if="!login">
+        <!-- <p>Enter Your authorized token</p> -->
         <div class="input-group mb-3">
           <input type="text" class="form-control" v-model="token" />
           <button class="btn btn-success" type="submit">Submit</button>
@@ -12,7 +16,8 @@
     <div class="chat">
       <div class="messages">
         <p v-for="message in messages" :key="message.id">
-        <span>{{message.name}}</span>:<span>{{message.message}}</span>
+          <span>{{ message.name }}</span
+          >:<span>{{ message.message }}</span>
         </p>
       </div>
       <form @submit.prevent="sendMessage()">
@@ -31,19 +36,35 @@ export default {
   data() {
     return {
       token: "",
+      login: false,
+      welcomeText: "Enter your authorized token",
+      error: "",
       inputMessage: "",
     };
   },
-  computed:{
-    messages(){
+  computed: {
+    messages() {
       return this.$store.state.messages;
-    }
+    },
+    user() {
+      return this.$store.state.user;
+    },
   },
   methods: {
     submitToken() {
       this.$store.commit("socketConnection", this.token);
       this.$store.commit("setUserInfo");
       this.$store.commit("receiveMessage");
+      setTimeout(() => {
+        if (this.user) {
+          this.login = true;
+          this.welcomeText = `Welcome ${this.user.name}`;
+          this.error = "";
+        } else {
+          this.error = "invalid token";
+          this.token = "";
+        }
+      }, 300);
     },
     sendMessage() {
       const CHAT_ROOM = "ChatRoomId";
